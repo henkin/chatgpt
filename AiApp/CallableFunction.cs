90ce1e605dbca851e35649e97f1be882107697ff
Paul Henkin <talktopaul@gmail.com>
2023-10-06 09:46:13 -0700

    Function calling through reflection

--- AiApp/CallableFunction.cs
+++ AiApp/CallableFunction.cs
@@ -4,21 +4,22 @@ namespace AiApp;
 
 public interface ICallableFunction
 {
-    public string Name { get; }
-    public string Description { get; }
-    public object Parameters { get; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public object Parameters { get; set; }
     Type ParamType { get; }
     Function GetFunction { get; }
-    public Task<object> Call(object param);
+    public Task<string> Call(object param);
+    
 }
 
 public class CallableFunction<TParams> : Function, ICallableFunction
 {
     public Type ParamType => typeof(TParams);
     public Function GetFunction => new(Name, Description, Parameters);
-    public Func<TParams, Task<object>> FuncCallback { get; set; } = null!;
+    public Func<TParams, Task<string>> FuncCallback { get; set; } = null!;
     
-    public async Task<object> Call(object param)
+    public async Task<string> Call(object param)
     {
         return await FuncCallback((TParams) param);
     }
